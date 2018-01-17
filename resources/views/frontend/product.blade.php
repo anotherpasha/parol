d1f0e5ec5b24731a4ef3cbead85dde3e80b79716
pashamahardika <me@pashamahardika.com>
2018-01-17 13:00:11 +0000

    split product details

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -59,124 +59,6 @@
 
     </section>
 
-    <section class="grey detail-summary">
-      <div class="container">
-          <div class="row">
-              <div class="col-xs-12 col-md-4">
-                <h3 class="color-primary flush-t">Ringkasan Perlindungan yang Kami Sediakan</h3>
-                <hr class="primary left"/>
-              </div>
-              <div class="col-xs-12 col-md-8 end">
-                <div class="row">
-                  <div class="col-xs-12">
-                    <dl class="">
-                      <dt class="summary-wrapper">
-                        <span class="ico">
-                          <img src="{{url('/uploads/dummies/icon-fire.png')}}" alt="">
-                        </span>
-                        <h5 class="color-primary title">Kebakaran</h5>
-                        <p class="color-dark-grey book description">Termasuk penyebaran api atau panas yang disebabkan oleh kekurang hati-hatian atau kesalahan tertanggung atau pihak lain, akibat menjalarnya api atau panas yang timbul sendiri (<i>self-combustion</i>) atau karena sifat barang itu sendiri atau karena hubungan arus pendek (<i>short circuit</i>).</p>
-                      </dt>
-                      <dt class="summary-wrapper">
-                        <span class="ico">
-                          <img src="{{url('/uploads/dummies/icon-lightning.png')}}" alt="">
-                        </span>
-                        <h5 class="color-primary title">Petir</h5>
-                        <p class="color-dark-grey book description">Kerusakan harta benda akibat petir yang menyebabkan api dan menyebabkan kerusakan.</p>
-                      </dt>
-                      <dt class="summary-wrapper">
-                        <span class="ico">
-                          <img src="{{url('/uploads/dummies/icon-bang.png')}}" alt="">
-                        </span>
-                        <h5 class="color-primary title">Ledakan</h5>
-                        <p class="color-dark-grey book description">Ledakan yang berasal dari harta benda yang diasuransikan. Artinya pelepasan energi yang terjadi secara mendadak yang dihasilkan dari gas atau uap.</p>
-                      </dt>
-
-                      <dt class="summary-wrapper">
-                        <span class="ico">
-                          <img src="{{url('/uploads/dummies/icon-plane.png')}}" alt="">
-                        </span>
-                        <h5 class="color-primary title">Kejatuhan Pesawat Terbang</h5>
-                        <p class="color-dark-grey book description">Adalah benturan fisik antara pesawat terbang termasuk helikopter atau segala sesuatu jatuh dari padanya dengan harta benda yang Anda asuransikan.</p>
-                      </dt>
-
-                      <dt class="summary-wrapper">
-                        <span class="ico">
-                          <img src="{{url('/uploads/dummies/icon-smoke.png')}}" alt="">
-                        </span>
-                        <h5 class="color-primary title">Asap</h5>
-                        <p class="color-dark-grey book description">Kerusakan bagian dari harta benda yang berasal dari kebakaran harta benda yang Anda asuransikan.</p>
-                      </dt>
-
-
-                    </dl>
-                  </div>
-                </div>
-              </div>
-          </div>
-      </div>
-      <div class="half-center-scroll-icon bottom">
-        <a class="scroll" href="javascript:;"><img src="{{url('/images/icon-scroll-blue.png')}}"/></a>
-      </div>
-
-    </section>
-
-      <section class="with-scroll-bottom  guarantee-summary  ">
-            <!-- Tabs pane -->
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12 col-md-4">
-                      <h3 class="color-primary flush-t">Jaminan Tambahan</br> atau Perluasan</h3>
-                      <hr class="primary left"/>
-                    </div>
-                    <div class="col-xs-12 col-md-8">
-                      <p class="color-dark-grey book">
-                          Dengan tambahan premi, maka tambahan Perlindungan Rumah & Harta Benda dapat diperluas dengan jaminan tambahan yang diinginkan terhadap kerusakan akibat:
-                      </p>
-                      <ul class="dotted-list ">
-                        <li>Kerusuhan, Pemogokan, Perbuatan Jahat, Huru-hara;</li>
-                        <li>Tertabrak Kendaraan;</li>
-                        <li>Angin Topan, Badai, Banjir, dan Kerusakan Akibat Air;</li>
-                        <li>Tanah Longsor;</li>
-                        <li>Biaya-biaya pembersihan puing;</li>
-                        <li>Gempa bumi dan letusan gunung berapi (menggunakan jaminan
-                           polis terpisah) </li>
-                      </ul>
-
-                    </div>
-                </div>
-            </div>
-            <div class="half-center-scroll-icon bottom">
-              <a class="scroll" href="javascript:;"><img src="{{url('/images/icon-scroll-blue.png')}}"/></a>
-            </div>
-
-      </section>
-
-      <section class="with-scroll-bottom grey ">
-            <!-- Tabs pane -->
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12 col-md-4">
-                      <h3 class="color-primary flush-t">Risiko yang</br>Di kecualikan</h3>
-                      <hr class="primary left"/>
-                    </div>
-                    <div class="col-xs-12 col-md-8">
-                      <ul class="dotted-list ">
-                        <li>Gempa bumi atau letusan gunung berapi</li>
-                        <li>Pemogokan, kerusakan, kegaduhan sipil, perbuatan jahat</li>
-                        <li>Perang atau akibat dari pemberontakan bersenjata</li>
-                        <li>Reaksi energi nuklir</li>
-                        <li>Pembawaan sendiri harta benda yang diasuransikan</li>
-                      </ul>
-
-                    </div>
-                </div>
-            </div>
-            <div class="half-center-scroll-icon bottom">
-              <a class="scroll" href="javascript:;"><img src="{{url('/images/icon-scroll-blue.png')}}"/></a>
-            </div>
-
-      </section>
   </div>
   <div role="tabpanel" class="tab-pane" id="profile">
     <section class="with-scroll-bottom  product-summary  ">
@@ -238,11 +120,17 @@
 
     </section>
   </div>
+
+    @include('partials.product-summary')
+
+    @include('partials.product-extra')
+
+    @include('partials.product-excluded')
 </main>
 
 <!-- section contact -->
 @include('partials.contact-form')
 
-@include('partials.toggled-calculator')
+<!-- @include('partials.toggled-calculator') -->
 
 @endsection
---
+++ resources/views/partials/product-excluded.blade.php
@@ -0,0 +1,25 @@
+<section class="with-scroll-bottom grey ">
+            <!-- Tabs pane -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-4">
+                      <h3 class="color-primary flush-t">Risiko yang</br>Di kecualikan</h3>
+                      <hr class="primary left"/>
+                    </div>
+                    <div class="col-xs-12 col-md-8">
+                      <ul class="dotted-list ">
+                        <li>Gempa bumi atau letusan gunung berapi</li>
+                        <li>Pemogokan, kerusakan, kegaduhan sipil, perbuatan jahat</li>
+                        <li>Perang atau akibat dari pemberontakan bersenjata</li>
+                        <li>Reaksi energi nuklir</li>
+                        <li>Pembawaan sendiri harta benda yang diasuransikan</li>
+                      </ul>
+
+                    </div>
+                </div>
+            </div>
+            <div class="half-center-scroll-icon bottom">
+              <a class="scroll" href="javascript:;"><img src="{{url('/images/icon-scroll-blue.png')}}"/></a>
+            </div>
+
+      </section>
